357efbb57792323218b9146a8300eb1ca7ca87f1
TheCodingLibrary <69860054+TheCodingLibrary@users.noreply.github.com>
2020-08-18 17:06:40 +0200

    Add files via upload

---
+++ mathLibrary.py
@@ -0,0 +1,22 @@
+# # Imports and Modules in Python 
+
+# # 1. Create a Module
+
+# Math Library
+
+# Function to add two numbers and print out sum
+def add(num1, num2):
+    result = num1 + num2
+    print("Sum: ", result)
+    return result
+
+# Variables in a Module
+operators = {
+  "plus": "+",
+  "minus": "-",
+  "divide": "/"
+}
+
+# Every data type can be imported from a module
+channel = "The Coding Library"
+language = "Python"
